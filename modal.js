9f94e64ac021b9733559f24c8341ab876b4bd740
Paul <69437294+Escbrk@users.noreply.github.com>
2025-03-12 17:28:18 -0600

    some upd

--- modal.js
+++ modal.js
@@ -1,7 +1,8 @@
 const modal = document.querySelector(".backdrop");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
-// const modalOpenBtn = document.querySelector(".modal-open-btn");
+const modalOpenBtn = document.querySelector(".modal-open-btn");
 
 const toggleModal = () => modal.classList.toggle("is-hidden");
 
 modalCloseBtn.addEventListener("click", toggleModal);
+modalOpenBtn.addEventListener('click', toggleModal)
